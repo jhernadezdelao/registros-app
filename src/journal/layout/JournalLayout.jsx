4f1c4a555e8995168cdc0f07263cd962d19b5ff9
Juan Hernandez <juancarlos.hernandez2@nissan.com.mx>
2025-03-19 20:28:32 -0600

    add noteView, add NothingSelectedVew and neded components

--- src/journal/layout/JournalLayout.jsx
+++ src/journal/layout/JournalLayout.jsx
@@ -1,14 +1,14 @@
-import { Box } from "@mui/material";
-import { NavBar } from "../components";
+import { Box, Toolbar } from "@mui/material";
+import { NavBar, SideBar } from "../components";
 
 const drawerWidth = 240;
 export const JournalLayout = ({ children }) => {
   return (
     <Box sx={{ display: "flex" }}>
       <NavBar drawerWidth={drawerWidth} />
-      {/**SideBar */}
+      <SideBar drawerWidth={drawerWidth} />
       <Box component="main" sx={{ flexGrow: 1, p: 3 }}>
-        {/**Toolbar */}
+        <Toolbar />
         {children}
       </Box>
     </Box>
